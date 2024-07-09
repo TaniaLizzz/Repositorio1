f5803067ebc4f3dee89da0d7b147f6cbe300d82f
TaniaLizz <tlizethts13@gmail.com>
2024-07-08 21:14:06 -0500

    Cuarto commit

---
+++ 02-input.py
@@ -0,0 +1,8 @@
+# Preguntar al usuario qué está estudiando y mostrar la respuesta
+# print("Tu nombre es " + input("Digite su nombre: ")
+print("Estoy estudiando " + input("¿Qué estás estudiando? "))
+print()
+print("Yo vivo en " + input("¿En qué país vives? "))
+print()
+print("Tu nombre es " + input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "))
+
--- 02-strings.py
+++ 02-strings.py
@@ -1,8 +1,18 @@
 #Genera texto
-print('150 + 10')
-# ctrl + } para comentar
-#Concatenar
-print('Hola ' + 'Grace')
-print('Hola' + ' Grace')
-print('Hola' + ' ' + 'Grace')
-print("Me llamo \"Tania\"")
+# print('150 + 10')
+# # ctrl + } para comentar
+# #Concatenar
+# print('Hola ' + 'Grace')
+# print('Hola' + ' Grace')
+# print('Hola' + ' ' + 'Grace')
+# #Comillas
+# print("Me llamo \"Tania\"")
+# #Salto de linea
+# print("Esta es una linea y\nesta es otra linea")
+# #Tabular
+# print("\tEsta será una linea tabulada")
+
+#Práctica String 1
+print("Línea 1\nLínea 2\nLínea 3")
+print()
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
