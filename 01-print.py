77afd4b899fc6272ef04ebfdd9285426df623a6c
TaniaLizz <tlizethts13@gmail.com>
2024-07-08 20:28:31 -0500

    Tercer commit

--- 01-print.py
+++ 01-print.py
@@ -1,3 +1,7 @@
-print("Hola mundo")
-print(150 + 50)
-print(2 + 2)
+#print("Hola mundo")
+#print(150 + 50)
+#print(2 + 2)
+
+print("Me encanta estudiar Python")
+print('Estudiar con "Python Total" es super divertido')
+print(200+355)
---
+++ 02-strings.py
@@ -0,0 +1,8 @@
+#Genera texto
+print('150 + 10')
+# ctrl + } para comentar
+#Concatenar
+print('Hola ' + 'Grace')
+print('Hola' + ' Grace')
+print('Hola' + ' ' + 'Grace')
+print("Me llamo \"Tania\"")
