374cfae0ccfcfe1413427ddcf55f4e5403212a8b
TaniaLizz <tlizethts13@gmail.com>
2024-07-15 14:20:51 -0500

    Ejercicio nombre de cerveza

---
+++ cerveza.py
@@ -0,0 +1,14 @@
+print("Cerveza Artesanal: Creando Nombres con Tu Imaginación\n")
+
+# Se pide el nombre de un animal y un color favorito
+animal = input("Ingresa el nombre de tu animal favorito: ")
+color = input("Ingresa el nombre de tu color favorito: ")
+
+# Se ombina las palabras para crear el nombre de la cerveza
+nombre_cerveza = animal + " " + color
+
+# Imprimir el resultado en dos líneas con comillas
+print("\nEl nombre de tu cerveza es:")
+print(f'"{nombre_cerveza}"')
+
+
