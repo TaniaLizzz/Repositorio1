00a82921294fd2f4cf3a303f16759271c2244567
TaniaLizz <tlizethts13@gmail.com>
2024-07-15 14:44:56 -0500

    integer y float

---
+++ 05-integer_float.py
@@ -0,0 +1,12 @@
+# mi_numero = 1
+# print(mi_numero)
+# print(type(mi_numero))
+
+# mi_decimal = 5.4
+# print(type(mi_decimal))
+
+# suma = 50 + 5.4
+# print(type(suma))
+
+edad = input("Dime tu edad: ")
+print(type(edad))   #str ; todo lo que ingresa por teclado con input es texto
