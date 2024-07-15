a454478e81c3737b578273d58121a8d0aaf0fa8b
TaniaLizz <tlizethts13@gmail.com>
2024-07-15 14:45:27 -0500

    conversiones

---
+++ 06-conversiones.py
@@ -0,0 +1,14 @@
+#Conversion implicita
+# num1 = 20
+# num2 = 30.5
+
+# print(type(num1))
+# print(type(num2))
+
+#conversion explicita
+num1 = 5.8
+print(num1)
+print(type(num1))
+
+num2 = int(num1)
+print(type(num2))
