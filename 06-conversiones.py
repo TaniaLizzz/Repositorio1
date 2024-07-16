8beef1d78d6934abdf43b34f660f5e780208d28d
TaniaLizz <tlizethts13@gmail.com>
2024-07-15 19:22:46 -0500

    Ejemplo de conversiones

--- 06-conversiones.py
+++ 06-conversiones.py
@@ -12,3 +12,8 @@ print(type(num1))
 
 num2 = int(num1)
 print(type(num2))
+
+#Por lo general recibe un string, entones convertimos a int
+# edad = input("Dime tu edad: ")
+edad = int(input("Dime tu edad: "))
+print(type(edad))
