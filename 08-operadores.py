8528b5623821ce3bd4050c77646c3ba7ec4d3ca8
TaniaLizz <tlizethts13@gmail.com>
2024-07-15 20:14:07 -0500

    Ejercicios con operadores

---
+++ 08-operadores.py
@@ -0,0 +1,30 @@
+# x = 6
+# y = 2
+# print(f"{x} mas {y} es igual a {x+y}")
+# print(f"{x} menos {y} es igual a {x-y}")
+# print(f"{x} por {y} es igual a {x*y}")
+# print(f"{x} dividido entre {y} es igual a {x/y}")
+
+# z = 7
+# print(f"{z} división al piso de {y} es igual a {z//y}")
+# #7/2=3  3x2=6 + 1 = 7 --> 3 (cociente) &&  1 (módulo)
+# print(f"{z} modulo de {y} es igual a {z%y}")
+# print(f"{x} elevado a la {y} es igul a {x**y}")
+# print(f"La raíz cuadrada de {x} es {x**0.5}")
+
+#Práctica 1 
+num1 = 874
+num2 = 27
+cociente = num1 // num2 
+print(cociente)
+
+#Práctica 2
+num3 = 456
+num4 = 33
+modulo = num3 % num4
+print(modulo)
+
+#Práctica 3
+num5 = 783
+raiz_cuadrada = num5 ** 0.5
+print(raiz_cuadrada)
