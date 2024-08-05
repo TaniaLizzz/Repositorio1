55aee1a6e5411e4b06f43c68323c5d886c2bb4a2
TaniaLizz <tlizethts13@gmail.com>
2024-08-05 15:49:49 -0500

    Nuevas modificaciones flujos

--- 20-tomar_decisiones.py
+++ 20-tomar_decisiones.py
@@ -24,14 +24,60 @@
 # else:
 #     print("No es una mascota")
     
-edad = 15
-calificacion = 5
+# edad = 15
+# calificacion = 5
 
-if edad < 18:
-    print("Eres menor de edad")
-    if calificacion >= 3:
-        print("Aprobado")
-    else:
-        print("No aprobado")
+# if edad < 18:
+#     print("Eres menor de edad")
+#     if calificacion >= 3:
+#         print("Aprobado")
+#     else:
+#         print("No aprobado")
+# else:
+#     print("Eres adulto")
+    
+    
+#Practica control de flujo 1
+
+# num1 = int(input("Ingresa un número: "))
+# num2 = int(input("Ingresa otro número: "))
+
+# if num1 > num2:
+#     print(f"{num1} es mayor que {num2}")
+# elif num2 > num1:
+#     print("num2 es mayor que num1")
+# else:
+#     print("num1 y num2 son iguales")
+    
+#Practica control de flujo 2
+# edad = 16
+# tiene_licencia = False
+
+# if edad >= 18 and tiene_licencia:  #And es cuando ambas se cumplen y or solo una
+#     print("Puedes conducir")
+# elif edad >= 18 and not tiene_licencia:
+#     print("No puedes conducir aún. Debes tener 18 años y contar con una licencia")
+# else:
+#     print("No puedes conducir. Necesitas tener al menos 18 años")
+# print("\n")
+# #Opcional para que el usuario ingrese sus datos 
+# edad = int(input("Introduce tu edad: "))
+# tiene_licencia_input = input("¿Tienes licencia de conducir? (si/no): ").strip().lower()
+# tiene_licencia = tiene_licencia_input == 'si'
+
+# if edad >= 18 and tiene_licencia:
+#     print("Puedes conducir")
+# elif edad >= 18 and not tiene_licencia:
+#     print("No puedes conducir aún. Debes tener 18 años y contar con una licencia")
+# else:
+#     print("No puedes conducir. Necesitas tener al menos 18 años")
+
+edad = 16
+tiene_licencia = False
+
+if edad >= 18 and tiene_licencia: #Como acá contemplo lo de la licencia en elif ya no es necesario
+    print("Puedes conducir")
+elif edad < 18:
+    print("No puedes conducir aun, debes tener 18 y contar con una licencia")
 else:
-    print("Eres adulto")
+    print("No puedes conducir.Necesitas tener al menos 18 años")
---
+++ controlflujo.py
@@ -0,0 +1,13 @@
+habla_ingles = True 
+sabe_python = False 
+
+print("Cumples con los requisitos para postularte")
+print("Para postularte, necesitas saber programar en Python y tener conocimientos de inglés")
+print("Para postularte, necesitas tener conocimientos de inglés")
+print("Para postularte, necesitas saber programar en Python")
+
+if habla_ingles:
+    if sabe_python:
+            print("Cumples con los requisitos para postularte")
+else:
+    print("Para postularte, necesitas saber programar en Python")
